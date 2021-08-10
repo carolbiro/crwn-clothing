7af47811c1e8363b33df01c42850a32ed1000d8b
Biro Carol <birocarol@gmail.com>
2021-08-10 12:48:48 +0300

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,9 +3,7 @@ import { withRouter } from 'react-router-dom'
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
-    console.log({ title, imageUrl, size, history, linkUrl, match })
-    return (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div 
         className={`${size} menu-item`} 
         onClick={() => history.push(`${match.url}${linkUrl}`)}
@@ -21,6 +19,6 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
-)}
+)
 
 export default withRouter(MenuItem)
