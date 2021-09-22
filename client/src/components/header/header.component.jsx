6f700c59c9239a06c650b339224ea3dd5cbfce8d
Biro Carol <birocarol@gmail.com>
2021-09-22 10:25:35 +0300

    remove gitignore files

---
+++ client/src/components/header/header.component.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+import { selectCartHidden } from '../../redux/cart/cart.selectors'
+import { selectCurrentUser } from '../../redux/user/user.selectors'
+import { signOutStart } from '../../redux/user/user.actions'
+
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+import { 
+    HeaderContainer, 
+    LogoContainer, 
+    OptionsContainer, 
+    OptionLink 
+} from './header.styles'
+
+const Header = () => {
+    const dispatch = useDispatch()
+    const currentUser = useSelector(selectCurrentUser)
+    const hidden = useSelector(selectCartHidden)
+
+    return (
+        <HeaderContainer>
+            <LogoContainer to="/">
+                <Logo className='logo' />
+            </LogoContainer>
+            <OptionsContainer>
+                <OptionLink to='/shop'>
+                    SHOP
+                </OptionLink>
+                <OptionLink to='/contact'>
+                    CONTACT
+                </OptionLink>
+                {
+                    currentUser ?
+                    <OptionLink as='div' onClick={() => dispatch(signOutStart())}>SIGN OUT</OptionLink>
+                    :
+                    <OptionLink to='/signin'>SIGN IN</OptionLink>
+                }
+                <CartIcon />
+            </OptionsContainer>
+            {
+                hidden ? null : <CartDropdown />
+            }
+        </HeaderContainer>
+    )
+}
+
+export default Header
---
+++ client/src/redux/shop/shop.reducer.js
@@ -0,0 +1,33 @@
+import ShopActionsTypes from "./shop.types"
+
+const INITIAL_STATE = {
+    collections: null,
+    isFetching: false,
+    erroMessage: undefined
+}
+
+const shopReducer = (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        case ShopActionsTypes.FETCH_COLLECTIONS_START:
+            return {
+                ...state,
+                isFetching: true
+            }
+        case ShopActionsTypes.FETCH_COLLECTIONS_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                collections: action.payload                
+            }
+        case ShopActionsTypes.ETCH_COLLECTIONS_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                erroMessage: action.payload
+            }            
+        default:
+            return state
+    }
+}
+
+export default shopReducer
